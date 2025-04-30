9861a8563821fa35e0b75cb04ab6b2e673ff730d
Mahmudul Hasan Shihan <123587075+mh-shihan@users.noreply.github.com>
2025-04-30 09:35:50 +0600

    Create home.php

---
+++ Assignment/Assignment-3/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Lab Report Home</title>
+</head>
+<body>
+<nav class="bg-lime-500 p-4 shadow">
+    <ul class="flex space-x-6 justify-center text-white font-semibold">
+        <li><a href="home.php" class="hover:text-gray-300 transition">Home</a></li>
+        <li><a href="login.php" class="hover:text-gray-300 transition">Login</a></li>
+        <li><a href="registration.php" class="hover:text-gray-300 transition">Registration</a></li>
+    </ul>
+</nav>
+<h1 class="text-white font-bold text-4xl text-center my-4">Welcome Home</h1>
+
+</body>
+</html>
